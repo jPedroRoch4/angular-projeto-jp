5e3ce3984847327b882824c423cdd6617b748b60
jPedroRoch4 <jpedro.devops@gmail.com>
2023-06-28 19:41:44 -0300

    2023.06.28-2 Aplicativo modelo em angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,57 @@
+import { E404Component } from './pages/e404/e404.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  // Rota padrão → Página inicial → 'home'.
+  // ↓ Essa deve ser sempre a primeira rota.
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+
+  // Página 'Home'.
+  {
+    path: 'home',
+    title: 'Início',
+    component: HomeComponent
+  },
+
+  // Página 'Contatos'.
+  {
+    path: 'contacts',
+    title: 'Contatos',
+    component: ContactsComponent
+  },
+
+  // Página 'Sobre'.
+  {
+    path: 'about',
+    title: 'Sobre',
+    component: AboutComponent
+  },
+
+  // Página '404'
+  {
+    path: 'e404',
+    title: 'Erro 404',
+    component: E404Component
+  },
+
+  // Se a rota não existe, vai para a página 404.
+  // ↓ Essa deve ser sempre a última rota.
+  {
+    path: '**',
+    redirectTo: 'e404',
+    pathMatch: 'full'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { AboutComponent } from './pages/about/about.component';
+import { E404Component } from './pages/e404/e404.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { ThemeComponent } from './theme/theme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactsComponent,
+    AboutComponent,
+    E404Component,
+    ProfileComponent,
+    ThemeComponent
   ],
   imports: [
     BrowserModule,
